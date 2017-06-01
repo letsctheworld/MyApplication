50fbb3d0b461b1bba2cf6f959afebf768f7d1836
shibin <shibinkv54>
2017-06-01 23:55:05 +0530

    dfgdfg

--- app/src/main/java/com/example/shibin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shibin/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.shibin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //dfgdfgdfg
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();    
     }
 }
